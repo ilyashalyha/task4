2d828ad762f9cf061ce907a60a0c06d92193cdd3
Ilya <ilya.shalyha@itechart-group.com>
2022-06-23 12:14:18 +0300

    Set up cucumber reporter

--- src/test/java/onliner/tests/runner/TestRunner.java
+++ src/test/java/onliner/tests/runner/TestRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/java/onliner/tests/features"},
+        plugin = {"json:target/cucumber.json", "html:target/cucumber-report.html"},
         glue = {"onliner/tests"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
