c5cf3beda19db02f698d9d28f0367f5433bb1e8a
ilyashalyha <100991661+ilyashalyha@users.noreply.github.com>
2022-07-01 17:36:48 +0300

    Delete src/test directory

--- src/test/java/framework/elements/TextBox.java
+++
@@ -1,9 +0,0 @@
-package framework.elements;
-
-import org.openqa.selenium.By;
-
-public class TextBox extends BaseElement{
-    public TextBox(By loc, String nameOf) {
-        super(loc, nameOf);
-    }
-}
